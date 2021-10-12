09249d865170de3367e60248bbfd4d2054cbc6b7
kanhucharan97 <kanhucharanpanigrahi74@gmail.com>
2021-10-12 09:47:17 +0530

    collegeboy 3.0

--- app.js
+++ app.js
@@ -1,20 +1,91 @@
 const boys = [
-  { img: 'sagar', name: 'sagar swain', roll: 'AS2018000' },
-  { img: 'debasish', name: 'debasish sethy', roll: 'AS2018000' },
-  { img: 'aditya', name: 'aditya acharya', roll: 'AS2018000' },
-  { img: 'sReddy', name: 'sagar reddy', roll: 'AS2018000' },
-  { img: 'sBiswal', name: 'sagar biswal', roll: 'AS2018000' },
-  { img: 'kanhu', name: 'kanhu charan panigrahi', roll: 'AS2018000' },
-  { img: 'krushna', name: 'krushna chandra swain', roll: 'AS2018000' },
-  { img: 'siba', name: 'siba sundar nahak', roll: 'AS2018000' },
-  { img: 'sunil', name: 'sunil kumar gouda', roll: 'AS2018000' },
-  { img: 'gamanga', name: 'maheswar gamanga', roll: 'AS2018000' },
-  { img: 'rabi', name: 'rabi kumar swain', roll: 'AS2018000' },
-  { img: 'miku', name: 'miku kumar swain', roll: 'AS2018000' },
-  { img: 'sambhu', name: 'sambhu gantayat', roll: 'AS2018000' },
-  { img: 'jitendra', name: 'jitendra sahu', roll: 'AS2018000' },
-  { img: 'sDash', name: 'sagar dash', roll: 'AS2018000' },
+  { img: 'sagar', name: 'sagar swain', roll: 'AS2018068', nam: 'sagarswain' },
+  {
+    img: 'debasish',
+    name: 'debasish sethy',
+    roll: 'AS2018000',
+    nam: 'debasishsethy',
+  },
+  {
+    img: 'aditya',
+    name: 'aditya acharya',
+    roll: 'AS2018036',
+    nam: 'adityaacharya',
+  },
+  {
+    img: 'sReddy',
+    name: 'sagar reddy',
+    roll: 'AS2018035',
+    nam: 'sagarreddy',
+  },
+  {
+    img: 'sBiswal',
+    name: 'sagar biswal',
+    roll: 'AS2018000',
+    nam: 'sagarbiswal',
+  },
+  {
+    img: 'kanhu',
+    name: 'kanhu charan panigrahi',
+    roll: 'AS2018048',
+    nam: 'kanhucharanpanigrahi',
+  },
+  {
+    img: 'krushna',
+    name: 'krushna chandra swain',
+    roll: 'AS2018050',
+    nam: 'krushnachandraswain',
+  },
+  {
+    img: 'siba',
+    name: 'siba sundar nahak',
+    roll: 'AS2018070',
+    nam: 'sibasundarnahak',
+  },
+  {
+    img: 'sunil',
+    name: 'sunil kumar gouda',
+    roll: 'AS2018077',
+    nam: 'sunilkumargouda',
+  },
+  {
+    img: 'gamanga',
+    name: 'maheswar gamanga',
+    roll: 'AS2018000',
+    nam: 'maheswargamanga',
+  },
+  {
+    img: 'rabi',
+    name: 'rabi kumar swain',
+    roll: 'AS2018065',
+    nam: 'rabikumarswain',
+  },
+  {
+    img: 'miku',
+    name: 'miku kumar swain',
+    roll: 'AS2018000',
+    nam: 'mikukumarswain',
+  },
+  {
+    img: 'sambhu',
+    name: 'sambhu gantayat',
+    roll: 'AS2018069',
+    nam: 'sambhugantayat',
+  },
+  {
+    img: 'jitendra',
+    name: 'jitendra sahu',
+    roll: 'AS2018045',
+    nam: 'jitendrasahu',
+  },
+  { img: 'sDash', name: 'sagar dash', roll: 'AS2018067', nam: 'sagardash' },
 ]
+
+boys.sort(function (a, b) {
+  if (a.nam < b.nam) return -1
+  if (a.nam > b.nam) return 1
+  return 0
+})
 
 window.addEventListener('DOMContentLoaded', () => {
   displayItems(boys)
@@ -26,7 +97,7 @@ window.addEventListener('DOMContentLoaded', () => {
     const val = e.target.value.toLowerCase()
 
     const filteredBoys = boys.filter((boy) => {
-      return boy.name.includes(val) || boy.roll.includes(val)
+      return boy.name.includes(val) || boy.roll.toLowerCase().includes(val)
     })
     // console.log(val)
 
@@ -46,8 +117,12 @@ window.addEventListener('DOMContentLoaded', () => {
 const info = document.querySelector('.info')
 
 function displayItems(boyitems) {
-  let boysInfo = boyitems.map((item) => {
-    return ` 
+  let boysInfo = boyitems
+    .sort(function (a, b) {
+      return a - b
+    })
+    .map((item) => {
+      return ` 
         <div class="${item.img} child" data-id="${item.img}">
          <div class="pSect">
             <img src="${item.img}.jpg" alt="" height="141px" />
@@ -59,7 +134,7 @@ function displayItems(boyitems) {
             <h2>${item.roll}</h2>
           </div>
        </div>`
-  })
+    })
   boysInfo = boysInfo.join('')
   info.innerHTML = boysInfo
 }
